61c95314d59ae37ebf7db2e3bdaa6afe537614f0
Lee White <lwhite@sportsbasement.com>
2015-04-22 00:27:06 +0000

    added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'layouts#home'
+  root 'static_pages#home'
+
+  get 'static_pages/help'
+
+  # get 'signup'      'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

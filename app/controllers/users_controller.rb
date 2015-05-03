7345bd2af8281ed6363a6b7e989c5560734d67a1
Lee White <lwhite@sportsbasement.com>
2015-05-03 19:32:51 +0000

    added login/logout, made sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,13 @@ class UsersController < ApplicationController
   def create
       @user = User.new(user_params)
       if @user.save
-        
+        log_in @user
+        flash[:success] = "Aww yisss, your account has been created!"
+        redirect_to @user
       else
         render 'new'
       end
-    end
+  end
     
   private
     

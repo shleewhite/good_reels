3ea934139c82a826d79fe97a4cc4efab4b01dca8
Lee White <lwhite@sportsbasement.com>
2015-05-06 23:36:52 +0000

    finally finished usersgit add -A

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -28,6 +29,11 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
     
   private
     
@@ -36,8 +42,13 @@ class UsersController < ApplicationController
     end
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 end

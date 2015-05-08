bb0a67de9a4e5904b0f4627b8cff509332b1fcfb
Lee White <lwhite@sportsbasement.com>
2015-05-08 21:42:31 +0000

    added a movie form, but error messages stopped working because no longer knows what errors are. wtf

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,21 @@
 class MoviesController < ApplicationController
+    before_action :logged_in_user, only: [:create, :destroy]
+    
+    def create
+        @movie = current_user.movie.build(movie_params)
+        if @movie.save
+          flash[:success] = "You added a movie to your list!"
+          redirect_to root_url
+        else
+          render 'static_pages/home'
+        end
+    end
+    def destroy
+    end
+    
+    private
+    
+    def movie_params
+        params.require(:movie).permit(:content)
+    end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @movie = current_user.movies.build if logged_in?
   end
 
   def help
